05f64143285af879dc12e6c0884075f83faf6604
AmelRidzal~ <amelridzal37@gmail.com~>
2025-12-14 14:29:42 +0100

    2024 d16

--- 2024/Day16/d16.cpp
+++ 2024/Day16/d16.cpp
@@ -2,11 +2,21 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <queue>
+#include <limits>
+
 using namespace std;
 
 
 
-typedef unsigned long long int ull;
+typedef long long int ull;
+void goingUp(vector<vector<ull>>& , ull , ull , ull);
+void goingDown(vector<vector<ull>>& , ull , ull , ull);
+void goingLeft(vector<vector<ull>>& , ull , ull , ull);
+void goingRight(vector<vector<ull>>& , ull , ull , ull);
+bool checkRaskrsnica(vector<vector<ull>>& , ull , ull );
+
+ull posEX,posEY;
 
 int main(){
 
@@ -16,35 +26,195 @@ ifstream file("input");
         return 1;
     }
 
-    vector<vector<char>> grid;   
+    vector<vector<ull>> grid;   
     string line;
+    ull posX,posY;
 
     while (getline(file, line)) {
         if (line.empty()) continue;  
 
         stringstream ss(line);
-        vector<char> row;
+        vector<ull> row;
         char x;
         while (ss >> x) {
-            row.push_back(x);
+            if(x=='S'){
+                posX=grid.size();
+                posY=row.size();
+            }
+            if(x=='#'){
+                row.push_back(-1);
+            }
+            else if(x=='E'){
+                posEX=grid.size();
+                posEY=row.size();
+                row.push_back(numeric_limits<long long int>::max());
+            }
+            else{
+                row.push_back(numeric_limits<long long int>::max());
+            }
         }
-        //if (line.find('-') == string::npos) {readingFirst = false;}
 
         grid.push_back(row);
     }
-
     
-    for (const auto& row : grid) {
-        for (char x : row) cout << x << " ";
+    /*for (const auto& row : grid) {
+        for (ull x : row) cout << x<<" ";
         cout << "\n";
+    }*/
+    grid[posX][posY]=0;
+    if(grid[posX-1][posY]!=-1){
+        goingUp(grid, posX-1, posY, 1001);
     }
+    if(grid[posX+1][posY]!=-1){
+        goingDown(grid, posX+1, posY, 1001);
+    }
+    if(grid[posX][posY-1]!=-1){
+        goingLeft(grid, posX, posY-1, 1001);
+    }
+    if(grid[posX][posY+1]!=-1){
+        goingRight(grid, posX, posY+1, 1);
+    }
+    
+    /*for (int i=0;i<grid.size();i++) {
+        for (int j=0;j<grid.at(i).size();j++) {
+            if(grid[i][j]==-2){
+                cout<<grid[i-1][j]<<" "<<grid[i][j-1]<<" "<<grid[i+1][j]<<" "<<grid[i][j+1]<<endl;
+            }
+        }
+    }*/
+   cout<<grid[posEX][posEY]<<endl;
+    return 0;
+}
 
-    for(ull i=0;i<grid.size();i++){
-        for(ull j=0;j<grid.at(i).size();j++){
 
+
+void goingUp(vector<vector<ull>>& grid, ull posX, ull posY, ull score){
+    if(posX==posEX && posEY==posY){
+        if(grid[posX][posY]>=score)
+            grid[posX][posY]=score;
+        cout<<score<<endl;
+        return;
+    }
+    grid[posX][posY]=score;
+    score++;
+    if(grid[posX-1][posY]!=-1){
+        if(grid[posX-1][posY]>=score){
+            goingUp(grid, posX-1, posY, score);
 
         }
     }
+    score+=1000;
+    if(grid[posX][posY+1]!=-1){
+        if(grid[posX][posY+1]>=score){
+            goingRight(grid, posX, posY+1, score);
 
-    return 0;
+        }
+    }
+    if(grid[posX][posY-1]!=-1){
+        if(grid[posX][posY-1]>=score){
+            goingLeft(grid, posX, posY-1, score);
+
+        }
+    }
+    return;
 }
+
+
+
+void goingDown(vector<vector<ull>>& grid, ull posX, ull posY, ull score){
+    if(posX==posEX && posEY==posY){
+        if(grid[posX][posY]>=score)
+            grid[posX][posY]=score;
+        cout<<score<<endl;
+        return;
+    }
+    grid[posX][posY]=score;
+    score++;
+    if(grid[posX+1][posY]!=-1){
+        if(grid[posX+1][posY]>=score){
+            goingDown(grid, posX+1, posY, score);
+        }
+    }
+    score+=1000;
+    if(grid[posX][posY+1]!=-1){
+        if(grid[posX][posY+1]>=score){
+            goingRight(grid, posX, posY+1, score);
+        }
+    }
+    if(grid[posX][posY-1]!=-1){
+        if(grid[posX][posY-1]>=score){
+            goingLeft(grid, posX, posY-1, score);
+
+        }
+    }
+    return;
+}
+
+
+
+void goingRight(vector<vector<ull>>& grid, ull posX, ull posY, ull score){
+    if(posX==posEX && posEY==posY){
+        if(grid[posX][posY]>=score)
+            grid[posX][posY]=score;
+        cout<<score<<endl;
+        return;
+    }
+    grid[posX][posY]=score;
+    score++;
+    if(grid[posX][posY+1]!=-1){
+        if(grid[posX][posY+1]>=score){
+            goingRight(grid, posX, posY+1, score);
+
+        }
+    }
+    score+=1000;
+    if(grid[posX+1][posY]!=-1){
+        if(grid[posX+1][posY]>=score){
+            goingDown(grid, posX+1, posY, score);
+
+        }
+    }
+    if(grid[posX-1][posY]!=-1){
+        if(grid[posX-1][posY]>=score){
+            goingUp(grid, posX-1, posY, score);
+
+        }
+    }
+    return;
+}
+
+
+
+void goingLeft(vector<vector<ull>>& grid, ull posX, ull posY, ull score){
+    if(posX==posEX && posEY==posY){
+        if(grid[posX][posY]>=score)
+            grid[posX][posY]=score;
+        cout<<score<<endl;
+        return;
+    }
+    grid[posX][posY]=score;
+    score++;
+    if(grid[posX][posY-1]!=-1){
+        if(grid[posX][posY-1]>=score){
+            goingLeft(grid, posX, posY-1, score);
+
+        }
+    }
+    score+=1000;
+    if(grid[posX+1][posY]!=-1){
+        if(grid[posX+1][posY]>=score){
+            goingDown(grid, posX+1, posY, score);
+
+        }
+    }
+    if(grid[posX-1][posY]!=-1){
+        if(grid[posX-1][posY]>=score){
+            goingUp(grid, posX-1, posY, score);
+
+        }
+    }
+    return;
+}
+
+
+
