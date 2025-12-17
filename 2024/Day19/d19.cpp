765b50803b46bd828a48b16dbe41901a8e80c34e
AmelRidzal~ <amelridzal37@gmail.com~>
2025-12-17 20:33:22 +0100

    2024 day19

---
+++ 2024/Day19/d19.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+
+
+typedef unsigned long long int ull;
+
+
+    bool canBuildString(const string& s, const vector<string>& parts) {
+        int n = s.size();
+        vector<bool> dp(n + 1, false);
+        dp[0] = true;
+
+        for (int i = 0; i < n; i++) {
+            if (!dp[i]) continue;
+
+            for (const string& w : parts) {
+                int len = w.size();
+                if (i + len <= n && s.compare(i, len, w) == 0) {
+                    dp[i + len] = true;
+                }
+            }
+        }
+
+        return dp[n];
+    }
+
+
+int main(){
+
+ifstream file("input");
+    if (!file) {
+        cerr << "Could not open file!\n";
+        return 1;
+    }
+ 
+    vector<string> comp; 
+    vector<string> target; 
+    string line;
+    getline(file, line);
+    stringstream ss(line);
+    char c;
+    string s;
+    while(ss>>c){
+        if(c!=',' && c!=' '){
+            s=s+c;
+        }else if(c==','){
+            comp.push_back(s);
+            s.clear();
+        }
+    }
+    comp.push_back(s);
+
+    while (getline(file, line)) {
+        if (line.empty()) continue;  
+
+        stringstream ss(line);
+        ss>>s;
+        target.push_back(s);
+    }
+
+    ull rez=0;
+    for (string x : target){
+        if(canBuildString(x,comp)){
+            rez++;
+        }
+    }
+    cout<<rez;
+    
+    //for (string x : comp) cout <<"c "<< x <<endl;
+    //for (string x : target) cout << x <<endl;
+
+    
+
+    return 0;
+}
